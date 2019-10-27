89382ac7537c93b296d8e2a9a3b3383853578fdb
veenanitk <veena.nagar.92@gmail.com>
2019-10-27 18:44:41 +0530

    mess database created

--- database/hostel_management_system_Mess_allocation.sql
+++ database/hostel_management_system_Mess_allocation.sql
@@ -2,7 +2,6 @@
 --
 -- Host: localhost    Database: hostel_management_system
 -- ------------------------------------------------------
--- Server version 5.7.23-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,44 +15,36 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Student`
+-- Table structure for table `Room`
 --
 
-DROP TABLE IF EXISTS `Student`;
+DROP TABLE IF EXISTS `Mess_Allocation`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Student` (
-  `Student_id` varchar(255) NOT NULL,
-  `Fname` varchar(255) NOT NULL,
-  `Lname` varchar(255) NOT NULL,
-  `Mob_no` varchar(255) NOT NULL,
-  `Dept` varchar(255) NOT NULL,
-  `Year_of_study` varchar(255) NOT NULL,
-  `Pwd` longtext NOT NULL,
-  `Hostel_id` int(10) DEFAULT NULL,
-  `Room_id` int(10) DEFAULT NULL,
-  `Mess_id` int(10) DEFAULT NULL,
-  `Mess_card_id` int(10) DEFAULT NULL,
-  PRIMARY KEY (`Student_id`),
-  KEY `Hostel_id` (`Hostel_id`),
-  KEY `Room_id` (`Room_id`),
-  CONSTRAINT `Student_ibfk_1` FOREIGN KEY (`Hostel_id`) REFERENCES `Hostel` (`Hostel_id`),
-  CONSTRAINT `Student_ibfk_2` FOREIGN KEY (`Room_id`) REFERENCES `Room` (`Room_id`),
+CREATE TABLE `Mess_Allocation` (
+  `Mess_card_id` int(10) NOT NULL AUTO_INCREMENT,
+  `Mess_id` int(10) NOT NULL,
+  `Mess_card_No` int(10) NOT NULL,
+  `Allocated` tinyint(1) ,
+  PRIMARY KEY (`Mess_card_id`),
   KEY `Mess_id` (`Mess_id`),
-  KEY `Mess_card_id` (`Mess_card_id`),
-  CONSTRAINT `Student_ibfk_3` FOREIGN KEY (`Mess_id`) REFERENCES `Mess_id` (`Mess_id`),
-  CONSTRAINT `Student_ibfk_4` FOREIGN KEY (`Mess_card_id`) REFERENCES `Mess_card_id` (`Mess_card_id`)
+  CONSTRAINT `Mess_ibfk_1` FOREIGN KEY (`Mess_id`) REFERENCES `Mess` (`Mess_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Student`
+-- Dumping data for table `Room`
 --
 
-LOCK TABLES `Student` WRITE;
-/*!40000 ALTER TABLE `Student` DISABLE KEYS */;
-INSERT INTO `Student` VALUES ('B160497CS','Prajwal','Ghoradkar','09757318791','CSE','3','$2y$10$hGFfPexHXi.Ec.WVp3qK4.RG8yajZeO31LQ/aOeF6iRU9fk3IFrkq',NULL,NULL,NULL,NULL);
-/*!40000 ALTER TABLE `Student` ENABLE KEYS */;
+LOCK TABLES `Mess_Allocation` WRITE;
+/*!40000 ALTER TABLE `Mess_Allocation` DISABLE KEYS */;
+INSERT INTO `Mess_Allocation` VALUES (1,1,1,0),(2,1,2,0),(3,1,3,0),(4,1,4,0),(5,1,5,0),
+(6,2,1,0),(7,2,2,0),(8,2,3,0),(9,2,4,0),(10,2,5,0),
+(11,3,1,0),(12,3,2,0),(13,3,3,0),(14,3,4,0),(15,3,5,0),
+(16,4,1,0),(17,4,2,0),(18,4,3,0),(19,4,4,0),(20,4,5,0),
+(21,5,1,0),(22,5,2,0),(23,5,3,0),(24,5,4,0),(25,5,5,0),
+(26,6,1,0),(27,6,2,0),(28,6,3,0),(29,6,4,0),(30,6,5,0);
+/*!40000 ALTER TABLE `Mess_Allocation` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -65,4 +56,3 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-15 14:14:14
